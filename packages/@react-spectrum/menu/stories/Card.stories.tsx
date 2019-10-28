4075e144f09ad53a492d8fed7d924438c12e79d7
Rob Snow <rsnow@adobe.com>
2019-10-28 16:11:58 -0700

    pushing remaining changes

--- packages/@react-spectrum/menu/stories/Card.stories.tsx
+++ packages/@react-spectrum/menu/stories/Card.stories.tsx
@@ -80,11 +80,11 @@ storiesOf('Cards', module)
 
         <span style={{margin: '5px'}}>
         <Card slots={customStyles}>
-            <Avatar slot="preview" src="https://scontent-sjc3-1.cdninstagram.com/vp/061c1b0fa69e3f36c24710f8d5603655/5E500437/t51.2885-15/sh0.08/e35/s640x640/72625830_117633199385660_495143751973844448_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_cat=108" />
-            <Avatar slot="avatar" src="https://a5.behance.net/a9758425f0eaa6f4064d20ba73dfb7946a48f067/img/profile/no-image-138.png?cb=264615658" />
-            <Slot slot="title"><HStack align-items="center"><Heading grow>Rob Snow</Heading><More /></HStack></Slot>
-            <Section slot="body">Testing CSS Grid in Cards</Section>
-            <Section slot="footer">Night sky</Section>
+          <Avatar slot="preview" src="https://scontent-sjc3-1.cdninstagram.com/vp/061c1b0fa69e3f36c24710f8d5603655/5E500437/t51.2885-15/sh0.08/e35/s640x640/72625830_117633199385660_495143751973844448_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_cat=108" />
+          <Avatar slot="avatar" src="https://a5.behance.net/a9758425f0eaa6f4064d20ba73dfb7946a48f067/img/profile/no-image-138.png?cb=264615658" />
+          <Slot slot="title"><HStack align-items="center"><Heading grow>Rob Snow</Heading><More /></HStack></Slot>
+          <Section slot="body">Testing CSS Grid in Cards</Section>
+          <Section slot="footer">Night sky</Section>
         </Card>
         </span>
         <span style={{margin: '5px'}}>
